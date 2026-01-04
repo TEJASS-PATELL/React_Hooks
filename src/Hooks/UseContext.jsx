51aa2d1bed854cb5b2f2d28b6313de5669958294
Tejas <tejasspatell2@gmail.com>
2026-01-04 22:02:58 +0530

    learning usecontext

---
+++ src/Hooks/UseContext.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/UI/Footer'
+
+function UseContext() {
+  return (
+    <div className='main-container'>
+        <h1 className='main-heading'>Learning UseContext</h1>
+        {/* <h1><ins>new tag ins</ins> hello bro <del>new tag del</del></h1> */}
+        <Footer />
+    </div>
+  )
+}
+
+export default UseContext
