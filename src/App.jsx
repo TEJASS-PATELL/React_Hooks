fb729ef933387691bd0ce6af1ffdaa5bc4897994
Tejas <tejasspatell2@gmail.com>
2026-01-05 19:41:16 +0530

    learning usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import CustomHook from './Hooks/CustomHook'
 import UseCallback from './Hooks/UseCallback'
 import UseContext from './Hooks/UseContext'
 import UseEffect from './Hooks/UseEffect'
+import UseLayoutEffect from './Hooks/UseLayoutEffect'
 import UseMemo from './Hooks/UseMemo'
+import UseReducer from './Hooks/UseReducer'
 import UseRef from './Hooks/UseRef'
 import UseState from './Hooks/UseState'
 
@@ -15,7 +18,10 @@ function App() {
    {/* <UseRef /> */}
    {/* <UseMemo /> */}
    {/* <UseCallback /> */}
-   <UseContext />
+   {/* <UseContext /> */}
+   {/* <UseReducer /> */}
+   {/* <UseLayoutEffect /> */}
+   <CustomHook />
    </>
   )
 }
