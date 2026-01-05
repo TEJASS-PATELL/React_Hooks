ade4a4c83ea4aa1e5769551cbd3838c3a7394354
Tejas <tejasspatell2@gmail.com>
2026-01-05 19:41:37 +0530

    learning usereducer

---
+++ src/Hooks/UseReducer.jsx
@@ -0,0 +1,40 @@
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return { count: state.count + 1 };
+
+        case "DECREMENT":
+            return { count: state.count - 1 };
+
+        case "RESET":
+            return { count: 0 };
+
+        default:
+            return state;
+    }
+};
+
+function UseReducer() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <div className='main-container'>
+            <h1 className='main-heading'>Learning UseReducer - </h1>
+            <h1> Count: {state.count} </h1>
+
+            <div style={{ display: "flex", justifyContent: "center", gap: "12px",  }}>
+                <button className='button' onClick={() => dispatch({ type: "INCREMENT" })}>INC</button>
+
+                <button className='button' onClick={() => dispatch({ type: "DECREMENT" })}>DEC</button>
+
+                <button className='button' onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default UseReducer;
