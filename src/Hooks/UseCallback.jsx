8360a6a9bf90cec6009f2484c522eaafce7aae6e
Tejas <tejasspatell2@gmail.com>
2026-01-25 20:49:55 +0530

    learning useCallBack

--- src/Hooks/UseCallback.jsx
+++ src/Hooks/UseCallback.jsx
@@ -15,7 +15,21 @@ export default function UseCallback() {
 
   return (
     <div className='main-container'>
-      <h1 className='main-heading'>Learning UseCallBack</h1>
+      <h1 className="main-heading">Learning useCallback - </h1>
+
+      <p className="hook-definition">
+        useCallback is a React Hook that memoizes a function and returns the same
+        function instance unless its dependencies change, helping prevent
+        unnecessary re-renders.
+      </p>
+
+      <ul className="hook-listt">
+        <li>Passing callback functions to memoized child components.</li>
+        <li>Preventing re-creation of functions on every render.</li>
+        <li>Optimizing performance in components with frequent re-renders.</li>
+        <li>Using stable function references in dependency arrays.</li>
+      </ul>
+
       <Children handleProps={newCount} />
       <h1>Count is {count}</h1>
       <button className="button" onClick={handletimer}>Timer</button>
